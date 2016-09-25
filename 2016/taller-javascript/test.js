5d9dcf1daed82cc6ed52fe6e27e9e70d4bdf3b9a
Miguel Gagliardo <mgagliardo@edrans.com>
2016-09-25 17:17:45 -0300

    First commit on 2016 modules and workshops

---
+++ 2016/taller-javascript/test.js
@@ -0,0 +1,25 @@
+describe("un cliente", function() {
+  var manuel;
+
+  beforeEach(function() {
+    manuel = new Cliente();
+    manuel.comprar(100);
+  });
+
+  it('cuando compra deberia aumentar la deuda', function() {
+    expect(manuel.deuda).toBe(100);
+  });
+
+  it('tiene el 30% de sus ingresos monto maximo de credito', function() {
+    expect(manuel.montoMaximoCredito()).toBe(3000);
+  });
+
+  it("cambio el monto maximo de credito para adaptarlo a la mitad de los ingresos - deuda", function() {
+    manuel.deuda = 1000;
+    manuel.montoMaximoCredito = function() {
+      return 0.5 * (this.ingreso - this.deuda);
+    };
+    expect(manuel.montoMaximoCredito()).toBe(4500);
+  });
+
+});
